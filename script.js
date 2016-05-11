cf76513df27a266a0f3e875e27cbe968efc04c4d
HQ <hq@lucidarchive.com>
2016-05-10 22:08:38 -0700

    adding /g/:gid guides

--- script.js
+++ script.js
@@ -57,6 +57,11 @@ vpApp.filter('bandcampEmbed', function($sce) {
   })
   .filter('decodeURI', function() {
     return window.decodeURI;
+  })
+  .filter('char',function(){
+    return function(input){
+      return String.fromCharCode(0x40 + parseInt(input,10));
+    };
   });
 
 vpApp.controller('MainController', function($scope, $http, $q) {
@@ -85,22 +90,39 @@ vpApp.controller('MainController', function($scope, $http, $q) {
     //normalize
     var db = [];
     var head = table[0];
+    var guides = [];
+    
+    //guide scope
+    var start = head.indexOf('UID');
+    var end = head.indexOf('Art');
+    
+    $scope.guideFields = head.slice(start, end); // 0 is all albums
+    $scope.guideUrls = tableInput[0].slice(start, end).map(function(a) {
+      return a.split('"')[1];
+    });
+    
+    //db scope
 
     for (r = 1; r < table.length; r++) {
       if (!table[r])
         continue;
+      
       var dbRow = {};
+      
       for (c = 0; c < head.length - 1; c++) {
+        
         if (!tableInput[r][c])
           continue;
+        
         var val;
-        if ('Art'.indexOf(head[c]) === 0) {
+        if ('Art' === head[c]) {
           var arr = tableInput[r][c].split('"');
           dbRow[head[c]] = arr[1];
           dbRow.Thumbnail = arr[3] || arr[1];
         }
-        else
+        else {
           dbRow[head[c]] = table[r][c];
+        }
       }
       dbRow[head[head.length - 1]] = table[r].slice(c);
 
@@ -136,6 +158,67 @@ vpApp.controller('DetailController', function($scope, $filter, $routeParams) {
   });
 });
 
+vpApp.controller('GuideController', function($scope, $filter, $routeParams) {
+  $scope.$parent.dbPromise.then(function() {
+    var guide = $scope.$parent.guideFields[$routeParams.gid];
+    $scope.guide = guide;
+    $scope.guideUrl = $scope.$parent.guideUrls[$routeParams.gid];
+    
+    var filter = {};
+    filter[guide] = '!!';
+    var albums = $filter('filter')($scope.$parent.db, filter);
+    
+    var height = 0;
+    var width = 0;
+    for (var i = 0; i < albums.length; i++) {
+      
+      //note: x and y are 1-indexed
+      
+      var xy = albums[i][guide];
+      xy = xy.trim().split(/\s+/g);
+      xy = xy[xy.length - 1];
+      
+      var y = xy.match(/\d+|[A-Z]+/)[0];
+      var x = xy.substr(y.length);
+      
+      if (isNaN(y))
+        y = y.charCodeAt(0) - 0x40;
+      if (isNaN(x))
+        x = x.charCodeAt(0) - 0x40;
+      
+      y = parseInt(y, 10);
+      x = parseInt(x, 10);
+      
+      if (y > height)
+        height = y;
+      if (x > width)
+        width = x;
+      
+      albums[i].y = y - 1;
+      albums[i].x = x - 1;
+    }
+    
+    //x and y 0-indexed
+    
+    $scope.height = height;
+    $scope.rows = [];
+    for (var i = 0; i < height; i++)
+      $scope.rows[i] = i;
+    
+    $scope.width = width;
+    $scope.cols = [];
+    for (var i = 0; i < width; i++)
+      $scope.cols[i] = i;
+    
+    $scope.albums = [];
+    for (var yi = 0; yi < height; yi++) {
+      $scope.albums[yi] = [];
+      for (var xi = 0; xi < width; xi++)
+        $scope.albums[yi][xi] = $filter('filter')(albums, { y: yi, x: xi }, true)[0]; // true -> exact match
+    }
+  });
+});
+
 vpApp.config(function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'partials/list.html',
@@ -146,6 +229,9 @@ vpApp.config(function($routeProvider) {
   }).when('/a/:uid', {
     templateUrl: 'partials/detail.html',
     controller: 'DetailController'
+  }).when('/g/:gid', {
+    templateUrl: 'partials/guide.html',
+    controller: 'GuideController'
   }).otherwise({
     redirectTo: '/'
   });
